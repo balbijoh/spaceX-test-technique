3a358085ea4c03e7af1b4d08d428e4c12544d65f
balbijoh <johanne.balbin@gmail.com>
2022-05-12 21:49:58 +0200

    Modif card latest launch +add CSS

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-/* Ce que j'aimerais afficher :
-    - FAIT : les positions des launchpads et landpads sur une carte
-    - le nombre et le type de fusées envoyées chaque année
-    - filtrer les fusées par launchpads et landpads
-    - la distance à laquelle se trouve le roadster par rapport à la Terre et sa date de lancement
-*/
-
-const latestLaunch = document.querySelector("#latest-launch-data");
-
 
     // latest launch : name and date
+
+const latestLaunch = document.querySelector("#latest-launch-datas");
+
 fetch("https://api.spacexdata.com/v4/launches/latest")
     .then(response => response.json())
     .then(response => {
-        latestLaunch.textContent = `Dernier envoi : ${response.name}, le ${response.date_utc}`;
+        latestLaunch.innerHTML = 
+            `<p><b>Nom</b> : ${response.name}</p>
+            <p><b>Date</b> : ${response.date_utc}</p>
+            <p>Pour voir la vidéo du lancement, cliquez <a class="media-link" target="_blank" title="Lien vers Youtube" href="${response.links.webcast}">ici</a>
+             ou utilisez le lien suivant :</p>
+             <p class="media-link"><a class="media-link" target="_blank" title="Lien vers Youtube" href="${response.links.webcast}">${response.links.webcast}</a></p>`;
     })
 
 
     // leaflet step 3
+
 let map = L.map('map').setView([51.505, -0.09], 13);
 
+
     // leaflet step 4 (doesn't work, replaced with an example from stackoverflow)
+
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
     // custom icon for launchpads markers
+
 let rocketIcon = L.icon({
     iconUrl: 'img/rocket_icon.png',
     iconSize: [20, 50]
 });
 
+
     // launchpads placed on the map + pop-ups
+
 fetch("https://api.spacexdata.com/v4/launchpads/query", {method: "POST"})
     .then(response => response.json())
     .then(response => {
@@ -42,7 +48,9 @@ fetch("https://api.spacexdata.com/v4/launchpads/query", {method: "POST"})
     map.setView([firstLaunchpad.latitude, firstLaunchpad.longitude], 12);
     })
 
+    
     // landpads placed on the map + pop-ups
+
 fetch("https://api.spacexdata.com/v4/landpads/query", {method: "POST"})
     .then(response => response.json())
     .then(response => {
