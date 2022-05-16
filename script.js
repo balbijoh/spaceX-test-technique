4b17d4658ad68a6f9678f58e80a4131d9baa021b
balbijoh <98465603+balbijoh@users.noreply.github.com>
2022-05-16 20:25:01 +0200

    Add rockets table

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
     // latest launch : name and date
 
 const latestLaunch = document.querySelector("#latest-launch-datas");
+const rocketsTable = document.querySelector("#rockets-table");
+
 
 fetch("https://api.spacexdata.com/v4/launches/latest")
     .then(response => response.json())
@@ -60,3 +62,59 @@ fetch("https://api.spacexdata.com/v4/landpads/query", {method: "POST"})
         })
     })
             
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+    // Rockets : tableau avec le nom des fusées, la date de leur premier vol, le nombre de vols
+    // en tout et le nombre de vols en 2022 pour chacune
+
+// let rockets = [];
+
+// class Rocket {
+//     constructor(rocketName, rocketId, firstFlight, numberOfFlights){
+//         this.rocketName = rocketName;
+//         this.rocketId = rocketId;
+//         this.firstFlight = firstFlight;
+//         this.numberOfFlights = numberOfFlights;
+//     }
+// }
+
+// fetch("https://api.spacexdata.com/v4/rockets/query", {method: "POST"})
+//     .then(response => response.json())
+//     .then(response => {
+//         response.docs.forEach(doc => {
+//             let undefinedRocket = new Rocket(doc.name, doc.id, doc.first_flight);
+//             rockets.unshift(undefinedRocket);
+//         });
+//     })
+
+// // console.log(rockets);
+
+// console.log(rockets);
+
+
+// fetch("https://api.spacexdata.com/v4/launches/query", {method: "POST"})
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+
+
+// Pour chaque index de l'API, si "rocket" correspond à "rocketId" de l'élément n de mon
+// array rockets, avec this.numberOfFlights++
+
+
+
+fetch("https://api.spacexdata.com/v4/rockets/query", {method: "POST"})
+    .then(response => response.json())
+    .then(response => {
+        response.docs.forEach(doc => {
+            let headerRocketName = document.createElement('th');
+            headerRocketName.innerHTML = `${doc.name}`;
+            rocketsTable.appendChild(headerRocketName);
+
+            let rowId = document.createElement('tr');
+            let rowIdData = document.createElement('td');
+            rowIdData.innerHTML = `${doc.id}`;
+            rowId.appendChild(rowIdData);
+            rocketsTable.appendChild(rowId);
+        });
+    })
